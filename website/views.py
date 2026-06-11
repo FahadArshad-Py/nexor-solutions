3725a6d2e74a9f7cbce79f94b6f42d8100e1bd34
Muhammad Fahad Arshad <fon.fa78@gmail.com>
2026-06-11 18:04:00 +0500

    Add temporary admin creation route

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from .models import ContactMessage
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 
 def home(request):
@@ -41,3 +43,14 @@ def contact(request):
 
 def nexor(request):
     return render(request, "website/nexor.html")
+
+def create_admin(request):
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@nexorsolutions.com",
+            password="ChangeThisPassword123"
+        )
+        return HttpResponse("Admin created successfully.")
+
+    return HttpResponse("Admin already exists.")
