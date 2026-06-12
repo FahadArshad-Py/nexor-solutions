bc6f4baf3a962d6422d62b39672df2e281e6dfd2
Muhammad Fahad Arshad <fon.fa78@gmail.com>
2026-06-12 19:27:35 +0500

    Improved Mesh Particles on phones

--- website/static/website/js/script.js
+++ website/static/website/js/script.js
@@ -57,4 +57,42 @@ tsParticles.load("particles-js", {
         color: "transparent"
     }
 });
+const isMobile = window.innerWidth <= 768;
 
+tsParticles.load("particles-js", {
+    particles: {
+        number: {
+            value: isMobile ? 40 : 200
+        },
+
+        color: {
+            value: "#ffffff"
+        },
+
+        size: {
+            value: isMobile ? 2 : 3
+        },
+
+        links: {
+            enable: true,
+            distance: isMobile ? 120 : 170,
+            color: "#ffffff",
+            opacity: 0.2,
+            width: 1
+        },
+
+        move: {
+            enable: true,
+            speed: isMobile ? 0.2 : 0.5
+        }
+    },
+
+    interactivity: {
+        events: {
+            onHover: {
+                enable: !isMobile,
+                mode: "attract"
+            }
+        }
+    }
+});
